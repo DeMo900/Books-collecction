5d5b2b4aa39210d53784ffc5c57763a557c00bff
DeMo900 <dodoadam893@gmail.com>
2025-11-12 19:58:44 +0200

    added the email tp the schema

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const mongodb = require("mongoose")
 const user = new mongodb.Schema({
 username:{type : String , unique : true, minlength:3 , maxlength:25 , required:true , match : /^[A-Za-z0-9._\- ]+$/},
 password:{type : String , minlength : 8 , maxlength : 80 , required : true , match : /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,80}$/},
+email : {type : String , unique : true , maxlength : 50 , required : true , match : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/},
 role:{type : String , enum : [`creator`,`user`] , default : `user` , required : true },
 createdAt:{type:Date , default:Date.now()}
 })
