f1b549d20d9b09335d1ecfafbf03f281306f0f0d
DeMo900 <dodoadam893@gmail.com>
2025-11-13 17:48:49 +0200

    rendering the pages

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+//requiring
+const um = require("../models/user")
+
+//GET signup
+exports.Getsignup = (req,res)=>{
+res.render("signup")
+}
+//Get signin
+exports.Getsignin = (req,res)=>{
+res.render("signin")
+}
--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,10 @@ const homecontroller = require("/home/adam/coding/Books-collecction/controllers/
 const books = require("/home/adam/coding/Books-collecction/controllers/books.js")
 //add book
 const addbook = require("/home/adam/coding/Books-collecction/controllers/add-books.js")
+//auth
+const { Getsignup , Getsignin} = require("../controllers/auth");
 //errors
-const errors = require("/home/adam/coding/Books-collecction/controllers/errors.js")
+const errors = require("/home/adam/coding/Books-collecction/controllers/errors.js");
 /////////////////////////////////////
 //home
 router.get("/",homecontroller.Gethome)
@@ -17,7 +19,6 @@ router.get("/",homecontroller.Gethome)
 router.get("/books",books.Getbooks)
 router.post("/books/search",books.searchbook)
 //add book
-
 router.get("/books/add-book",addbook.Getaddbook)
 router.post("/books/add-book",
 body("title").notEmpty().isLength({min:4,max:80}).withMessage("you should type atleast 4 letters in title :("),
@@ -27,6 +28,10 @@ body("genre").notEmpty().isIn(["Fiction","Non-Fiction","Fantasy","Science Fictio
 .isLength({min:6 , max:16}).withMessage("please pick a genre"),
 body("publisyear").isLength({max:4}).isNumeric().notEmpty().withMessage("publish year is required")
 ,addbook.createbook)
+//signup and signin
+router.get("/signup",Getsignup)
+router.get("/signin",Getsignin)
+
 //errors
 router.get("/500",errors.get500)
 //about 
